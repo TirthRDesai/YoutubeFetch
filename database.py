d07de6d5fe24b3fa7e29fdeba9cc0333be706091
TIRTH <tirthrdesai05@gmail.com>
2023-11-05 06:05:56 -0500

    push

--- database.py
+++ database.py
@@ -2,22 +2,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+cred = credentials.Certificate(
+    'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
+})
+
 
 def main(userUrl):
 
-    # As an admin, the app has access to read and write all data, regradless of Security Rules
-    # Fetch the service account key JSON file contents
-    if (not credentials.Certificate(
-            'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')):
-        cred = credentials.Certificate(
-            'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
-    if not firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
-    }):
-        # Initialize the app with a service account, granting admin privileges
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
-        })
     ref = db.reference()
     ref.set({
         'data': {
