5cc5b0c4a56744869be92b219704df1f91c284cc
TIRTH <tirthrdesai05@gmail.com>
2023-11-05 06:09:09 -0500

    push

--- database.py
+++ database.py
@@ -2,19 +2,20 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate(
-    'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
-# Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
-})
-
 
 def main(userUrl):
+    try:
+        cred = credentials.Certificate(
+            'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
+        # Initialize the app with a service account, granting admin privileges
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
+        })
 
-    ref = db.reference()
-    ref.set({
-        'data': {
-            'url': userUrl
-        }
-    })
+    except Exception as e:
+        ref = db.reference()
+        ref.set({
+            'data': {
+                'url': userUrl
+            }
+        })
