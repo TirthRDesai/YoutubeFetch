5f9d6a4cc4bb4037e79149f6362915cf9e2f3ee2
TIRTH <tirthrdesai05@gmail.com>
2023-11-05 07:41:17 -0500

    push

--- database.py
+++ database.py
@@ -2,23 +2,23 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-if len(firebase_admin._apps) == 0:
-    cred = credentials.Certificate(
-        'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
-    firebase_admin.initialize_app(cred, options={
-        'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
-    })
-else:
-    cred = credentials.Certificate(
-        'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
-    firebase_admin.initialize_app(cred, name="DATABASEPY", options={
-        'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
-    })
-
-db = db.reference("/")
-
 
 def main(userUrl):
+    if len(firebase_admin._apps) == 0:
+        cred = credentials.Certificate(
+            'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
+        firebase_admin.initialize_app(cred, options={
+            'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
+        })
+    else:
+        if "DATABASEPY" not in firebase_admin._apps.keys():
+            cred = credentials.Certificate(
+                'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
+            firebase_admin.initialize_app(cred, name="DATABASEPY", options={
+                'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
+            })
+
+    db = db.reference("/")
     print(firebase_admin._apps)
     print(db.get())
     db.set({
