bee4514ddee4dc45419e71f589892e328be8582b
TIRTH <tirthrdesai05@gmail.com>
2023-11-05 06:12:39 -0500

    push

--- database.py
+++ database.py
@@ -7,11 +7,10 @@ def main(userUrl):
     try:
         cred = credentials.Certificate(
             'ub-hackathon-2023-firebase-adminsdk-7l4y4-d2367ecf0b.json')
-        # Initialize the app with a service account, granting admin privileges
         firebase_admin.initialize_app(cred, {
             'databaseURL': 'https://ub-hackathon-2023.firebaseio.com/'
         })
-        ref = db.reference()
+        ref = db.reference("/")
         ref.set({
             'data': {
                 'url': str(userUrl)
